4b6cafea00892a98d4eb57a943ed5c600083aa96
Andi Miller <andi@andimiller.net>
2016-11-10 12:24:56 +0000

    don't build swagger-codegen

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 if [ ! -d blazescala ]; then
   git clone https://github.com/andimiller/swagger-codegen-blazescala blazescala
 fi
-if [ ! -d swagger-codegen ]; then
-  git clone https://github.com/swagger-api/swagger-codegen
+if [ ! -f swagger-codegen-cli-2.2.1.jar ]; then
+  wget http://jcenter.bintray.com/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar
 fi
-if [ ! -f swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar ]; then
-  cd swagger-codegen
-  mvn package
-  cd ..
-fi
-java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i https://esi.tech.ccp.is/latest/swagger.json -l async-scala -o client -t blazescala --api-package eveapi.esi.api --invoker-package eveapi.esi.client --model-package eveapi.esi.model --additional-properties clientName=EsiClient  --artifact-id esi-client --group-id eveapi
+java -jar swagger-codegen-cli-2.2.1.jar generate -i https://esi.tech.ccp.is/latest/swagger.json -l async-scala -o client -t blazescala --api-package eveapi.esi.api --invoker-package eveapi.esi.client --model-package eveapi.esi.model --additional-properties clientName=EsiClient  --artifact-id esi-client --group-id eveapi
 cp blazescala/*.scala client/src/main/scala/
