5fa78ce8b0b9c290418d8f9606584c00e504fb61
Kalu <info@zap.de.com>
2018-10-29 17:22:50 +0100

    revert to 2.2.1 and async-scala

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 if [ ! -d blazescala ]; then
   git clone https://github.com/andimiller/swagger-codegen-blazescala blazescala
 fi
-if [ ! -f swagger-codegen-cli-2.3.1.jar ]; then
-  wget http://jcenter.bintray.com/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar
+if [ ! -f swagger-codegen-cli-2.2.1.jar ]; then
+  wget http://jcenter.bintray.com/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar
 fi
 cat blazescala/client.header.mustache blazescala/*.scala blazescala/client.footer.mustache > blazescala/client.mustache
-java -jar swagger-codegen-cli-2.3.1.jar generate -i https://esi.tech.ccp.is/latest/swagger.json -l scala -o client -t blazescala --api-package eveapi.esi.api --invoker-package eveapi.esi.client --model-package eveapi.esi.model --additional-properties clientName=EsiClient  --artifact-id esi-client --group-id eveapi
-./scalafmt -i -f client --config scalafmt.conf
+java -jar swagger-codegen-cli-2.2.1.jar generate -i https://esi.tech.ccp.is/latest/swagger.json -l async-scala -o client -t blazescala --api-package eveapi.esi.api --invoker-package eveapi.esi.client --model-package eveapi.esi.model --additional-properties clientName=EsiClient  --artifact-id esi-client --group-id eveapi
+#./scalafmt -i -f client --config scalafmt.conf
